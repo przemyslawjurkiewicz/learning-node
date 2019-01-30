497fd60d3ce435ddeadd4a34db740612d7d07d4a
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-01-30 14:45:46 +0100

    Add EventEmiter

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-var OSInfo = require('./modules/OSInfo');
+var EventEmitter = require("events").EventEmitter;
+var OSInfo = require("./modules/OSInfo");
+
+var emitter = new EventEmitter();
+emitter.on("beforeCommand", function(instruction) {
+  console.log("You wrote: " + instruction + ", trying to run command");
+});
+emitter.on("afterCommand", function() {
+  console.log("Finished command");
+});
 
 process.stdin.setEncoding("utf-8");
-process.stdin.on("readable", function () {
-    var input = process.stdin.read();
-    if (input !== null) {
-        var instruction = input.toString().trim();
-        switch (instruction) {
-            case 'showVersion':
-                process.stdout.write(process.versions.node);
-                break;
-            case 'showLang':
-                process.stdout.write(process.env.lang);
-                break;
-            case 'getOSinfo':
-                OSInfo.print();
-                break;
-            case 'exit':
-                process.stdout.write('Quitting app!\n');
-                process.exit();
-            default:
-                process.stderr.write('Wrong instruction!\n');
-                break;
-        }
+process.stdin.on("readable", function() {
+  var input = process.stdin.read();
+  if (input !== null) {
+    var instruction = input.toString().trim();
+    emitter.emit("beforeCommand", instruction);
+    switch (instruction) {
+      case "showVersion":
+        console.log(process.versions.node);
+        break;
+      case "showLang":
+        console.log(process.env.lang);
+        break;
+      case "getOSinfo":
+        OSInfo.print();
+        break;
+      case "exit":
+        console.log("Quitting app!");
+        process.exit();
+      default:
+        process.stderr.write("Wrong instruction!\n");
+        break;
     }
+    emitter.emit("afterCommand");
+  }
 });
